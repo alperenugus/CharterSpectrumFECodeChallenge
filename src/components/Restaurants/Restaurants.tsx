d281eb382c9a18ce0a5723b5d1851f997299f9e3
alperenugus <alperenugus@gmail.com>
2020-12-14 13:41:31 -0700

    Created the components

---
+++ src/components/Restaurants/Restaurants.tsx
@@ -0,0 +1,10 @@
+import React, {FC, ReactElement} from 'react';
+
+const Restaurants : FC = (props) : ReactElement => {
+
+
+    return <div>Hello world</div>;
+
+}
+
+export default Restaurants;
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,10 @@
+import React, {FC, ReactElement} from 'react';
+
+const Table : FC = (props) : ReactElement => {
+
+
+    return <div>Hello world</div>;
+
+}
+
+export default Table;
